19680215fc6dfa1d77a2c5314d5b980ad6d91d80
Dominik Rafacz <dominik.rafacz@protonmail.com>
2022-11-13 21:51:02 +0000

    Add dashboard basic layout

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,32 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+var sidebar = document.getElementById("sidebar");
+var sidebarScreenHover = document.getElementById("sidebar-screen-hover");
+
+/*
+small screen hide by default show when has class show
+big screen show by default hide when has class hide
+ */
+
+window.dashboardSidebarToggle = function() {
+
+    if (document.body.clientWidth < 768) {
+        sidebar.classList.toggle("show");
+        sidebarScreenHover.classList.toggle("show");
+    } else {
+        sidebar.classList.toggle("hide");
+    }
+}
+
+window.addEventListener("resize", function(event) {
+    //sidebar.classList.contains("section-filter")
+    console.log(document.body.clientWidth + ' wide by ' + document.body.clientHeight+' high');
+
+    if (document.body.clientWidth < 768) {
+        sidebar.classList.remove("hide")
+    } else {
+        sidebar.classList.remove("show");
+        sidebarScreenHover.classList.remove("show");
+    }
+})
