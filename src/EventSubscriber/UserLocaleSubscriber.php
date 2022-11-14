15085d1f5c32132c36f6e2dbba650e9ff495f275
Dominik Rafacz <dominik.rafacz@protonmail.com>
2022-11-14 20:39:48 +0000

    Fix user language after login

---
+++ src/EventSubscriber/UserLocaleSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+
+class UserLocaleSubscriber implements EventSubscriberInterface
+{
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function onInteractiveLogin(InteractiveLoginEvent $event): void
+    {
+        /** @var User $user */
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if (null !== $user->getLanguage()) {
+            $this->requestStack->getSession()->set('_locale', $user->getLanguage());
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SecurityEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin',
+        ];
+    }
+}
