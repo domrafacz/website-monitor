5dd3add2c9fa6dd03f4ead61ff341f5b006a60b6
Dominik Rafacz <dominik.rafacz@protonmail.com>
2022-11-15 22:04:59 +0000

    Fix wrong translator locale when session is loaded from rememberme token

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -7,10 +7,11 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
+
 class LocaleSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private readonly string $defaultLocale
+        private readonly string $defaultLocale,
     ) {}
 
     public function onKernelRequest(RequestEvent $event): void
--- src/EventSubscriber/UserLocaleSubscriber.php
+++ src/EventSubscriber/UserLocaleSubscriber.php
@@ -8,15 +8,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\SecurityEvents;
+use Symfony\Component\Translation\LocaleSwitcher;
 
 class UserLocaleSubscriber implements EventSubscriberInterface
 {
-    private RequestStack $requestStack;
-
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->requestStack = $requestStack;
-    }
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly LocaleSwitcher $localeSwitcher
+    ) {}
 
     public function onInteractiveLogin(InteractiveLoginEvent $event): void
     {
@@ -25,6 +24,7 @@ class UserLocaleSubscriber implements EventSubscriberInterface
 
         if (null !== $user->getLanguage()) {
             $this->requestStack->getSession()->set('_locale', $user->getLanguage());
+            $this->localeSwitcher->setLocale($user->getLanguage());
         }
     }
 
