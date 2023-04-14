d9a65817955f15f4f121ac62a2bb80041ec31f9f
Dominik Rafacz <dominik.rafacz@protonmail.com>
2023-04-14 20:14:37 +0000

    Add missing migration

---
+++ migrations/Version20230408095054.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230408095054 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE website ADD cert_expiration_notifications TEXT DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN website.cert_expiration_notifications IS \'(DC2Type:simple_array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE website DROP cert_expiration_notifications');
+    }
+}
