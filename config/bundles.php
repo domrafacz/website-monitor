a124be714024bc9ef7f1e4364cdd883ea1f46b1c
Dominik Rafacz <dominik.rafacz@protonmail.com>
2023-06-08 20:26:32 +0000

    Fix composer dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
