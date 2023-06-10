c16f8b79a8b3d17e1f06d99bd1c394c8af162300
Dominik Rafacz <dominik.rafacz@protonmail.com>
2023-06-10 21:40:18 +0000

    Add users list in admin panel

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin', name: 'app_admin_')]
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'users')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function dashboard(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/users.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+}
