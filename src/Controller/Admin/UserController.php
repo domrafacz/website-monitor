35180c4c06f679ea53142f2d13be67054f09ad64
Dominik Rafacz <dominik.rafacz@protonmail.com>
2023-07-28 21:09:59 +0000

    Admin panel - edit user form

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -4,21 +4,50 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\Admin\UserEditType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/admin', name: 'app_admin_')]
 class UserController extends AbstractController
 {
+    public function __construct(
+        private readonly UserRepository      $userRepository,
+        private readonly TranslatorInterface $translator
+    )
+    {
+    }
+
     #[Route('/users', name: 'users')]
     #[IsGranted('ROLE_ADMIN')]
-    public function dashboard(UserRepository $userRepository): Response
+    public function dashboard(): Response
     {
         return $this->render('admin/users.html.twig', [
-            'users' => $userRepository->findAll(),
+            'users' => $this->userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/users/edit/{id}', name: 'users_edit')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->userRepository->save($user, true);
+            $this->addFlash('success', $this->translator->trans('flash_save_success'));
+            return $this->redirectToRoute('app_admin_users_edit', ['id' => $user->getId()]);
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'edit_form' => $form
         ]);
     }
 }
---
+++ src/Form/Admin/UserEditType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'email',
+            ])
+            ->add('quota', IntegerType::class, [
+                'label' => 'quota',
+            ])
+            ->add('add', SubmitType::class, [
+                'label' => 'save',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
